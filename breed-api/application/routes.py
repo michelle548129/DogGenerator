f53df1a88ecbf2a16c5a5c1a01f864699aa66c85
michelle548129 <michelle548129@gmail.com>
2022-04-08 07:10:48 +0000

    updates done 2

--- breed-api/application/routes.py
+++ breed-api/application/routes.py
@@ -2,7 +2,7 @@ from application import app
 from flask import jsonify
 from random import choice
 
-breeds = ['rottweiler', 'dalmatian', 'chihuahua', 'labrador', 'poodle', 'dachshund', 'boxer', 'pug', 'bulldog', 'doberman', 'maltese', 'cockapoo', 'maltipoo', 'bloodhound', 'chowchow', 'hound', 'collie', 'whippet', 'terrier', 'pomeranian', 'corgi', 'spaniel', 'mastiff', 'beagle', 'atika', 'retriever'']
+breeds = ['rottweiler', 'dalmatian', 'chihuahua', 'labrador', 'poodle', 'dachshund', 'boxer', 'pug', 'bulldog', 'doberman', 'maltese', 'cockapoo', 'maltipoo', 'bloodhound', 'chowchow', 'hound', 'collie', 'whippet', 'terrier', 'pomeranian', 'corgi', 'spaniel', 'mastiff', 'beagle', 'atika', 'retriever']
 
 @app.route('/breed', methods=['GET'])
 def get_breed():
